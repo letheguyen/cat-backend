ab05d916d24aeb6e37cdd8419a4b89fc685a21a9
NguyenLT89 <nguyenlt@relipasoft.com>
2023-04-26 18:08:01 +0700

    feat: create api sign up

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { APP_URL } = require('../constants')
 
-const { getUsers, SignIn } = require('../controllers')
+const { getUsers, SignIn, signUp } = require('../controllers')
 
 // App
 router.post(APP_URL.login, SignIn)
+router.post(APP_URL.signUp, signUp)
 
-
-router.get('/users', getUsers)
+// Admin
+router.get(APP_URL.users, getUsers)
 
 
 module.exports = router
